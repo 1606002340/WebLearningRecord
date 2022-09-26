329a582be2dbc0be76720177425c32b94694d1c8
WCY <1606002340@qq.com>
2022-09-26 14:06:20 +0800

    关于vue_test上传

---
+++ 9.Vue/vue_test/17_src_过度与动画/components/Test.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+		<button @click="isShow = !isShow">显示/隐藏</button>
+		<transition name="hello" appear>
+			<h1 v-show="isShow">你好啊！</h1>
+		</transition>
+	</div>
+</template>
+
+<script>
+	export default {
+		name:'Test',
+		data() {
+			return {
+				isShow:true
+			}
+		},
+	}
+</script>
+
+<style scoped>
+	h1{
+		background-color: orange;
+	}
+	/* 上面不写名字（name）是v-xxxx   写了就用name的名字 名字-xxx */
+	.hello-enter-active{
+		animation: atguigu 0.5s linear;
+	}
+
+	.hello-leave-active{
+		animation: atguigu 0.5s linear reverse;
+	}
+
+	@keyframes atguigu {
+		from{
+			transform: translateX(-100%);
+		}
+		to{
+			transform: translateX(0px);
+		}
+	}
+</style>
